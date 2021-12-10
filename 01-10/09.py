1c2e377ad0dc5f6dee9dfc77b15a7df55a151c2a
yveso <yveso@users.noreply.github.com>
2021-12-10 18:43:33 +0100

    Day 9 refactored

--- 01-10/09.py
+++ 01-10/09.py
@@ -13,29 +13,14 @@ low_points = []
 for y, row in enumerate(height_map):
     for x, height in enumerate(row):
         adjacent_locations = []
-        if y == 0 or y == len(height_map) - 1 or x == 0 or x == len(row) - 1:
-            if y != 0:
-                # has top
-                adjacent_locations.append(height_map[y - 1][x])
-            if y != len(height_map) - 1:
-                # has bottom
-                adjacent_locations.append(height_map[y + 1][x])
-            if x != 0:
-                # has left
-                adjacent_locations.append(height_map[y][x - 1])
-            if x != len(row) - 1:
-                # has right
-                adjacent_locations.append(height_map[y][x + 1])
-
-        else:
-            adjacent_locations.extend(
-                [
-                    height_map[y - 1][x],
-                    height_map[y + 1][x],
-                    height_map[y][x - 1],
-                    height_map[y][x + 1],
-                ]
-            )
+        if y != 0:
+            adjacent_locations.append(height_map[y - 1][x])
+        if y != len(height_map) - 1:
+            adjacent_locations.append(height_map[y + 1][x])
+        if x != 0:
+            adjacent_locations.append(height_map[y][x - 1])
+        if x != len(row) - 1:
+            adjacent_locations.append(height_map[y][x + 1])
 
         if all(height < adj_loc for adj_loc in adjacent_locations):
             low_points.append(height)
@@ -49,29 +34,14 @@ basin_sizes = []
 for y, row in enumerate(height_map):
     for x, height in enumerate(row):
         adjacent_locations = []
-        if y == 0 or y == len(height_map) - 1 or x == 0 or x == len(row) - 1:
-            if y != 0:
-                # has top
-                adjacent_locations.append(height_map[y - 1][x])
-            if y != len(height_map) - 1:
-                # has bottom
-                adjacent_locations.append(height_map[y + 1][x])
-            if x != 0:
-                # has left
-                adjacent_locations.append(height_map[y][x - 1])
-            if x != len(row) - 1:
-                # has right
-                adjacent_locations.append(height_map[y][x + 1])
-
-        else:
-            adjacent_locations.extend(
-                [
-                    height_map[y - 1][x],
-                    height_map[y + 1][x],
-                    height_map[y][x - 1],
-                    height_map[y][x + 1],
-                ]
-            )
+        if y != 0:
+            adjacent_locations.append(height_map[y - 1][x])
+        if y != len(height_map) - 1:
+            adjacent_locations.append(height_map[y + 1][x])
+        if x != 0:
+            adjacent_locations.append(height_map[y][x - 1])
+        if x != len(row) - 1:
+            adjacent_locations.append(height_map[y][x + 1])
 
         if all(height < adj_loc for adj_loc in adjacent_locations):
             # https://de.wikipedia.org/wiki/Floodfill
